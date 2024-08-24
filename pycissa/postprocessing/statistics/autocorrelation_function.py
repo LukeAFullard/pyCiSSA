f0b8a87ac2740e5eb7187599cb8aae583a10ed4f
LukeAFullard <LukeAFullard@gmail.com>
2024-08-24 23:07:15 +1200

    adding acf, pacf plots

--- pycissa/postprocessing/statistics/autocorrelation_function.py
+++ pycissa/postprocessing/statistics/autocorrelation_function.py
@@ -108,3 +108,140 @@ def calculate_partial_autocorrelation_function(x:      np.ndarray,
                                     method=method,
                                     alpha=alpha)
     return pact,confidence_interval
+
+
+
+###############################################################################
+###############################################################################
+###############################################################################
+import matplotlib.pyplot as plt
+def plot_time_series_and_acf_pacf(t:                np.ndarray, 
+                                  x:                np.ndarray, 
+                                  acf_lags:         int|list|None=None, 
+                                  pacf_lags:        int|list|None=None, 
+                                  alpha:            float=0.05, 
+                                  use_vlines:       bool=True, 
+                                  adjusted:         bool=False, 
+                                  fft:              bool=False,
+                                  missing:          str='none',
+                                  zero:             bool=True, 
+                                  auto_ylims:       bool=False, 
+                                  bartlett_confint: bool=True,
+                                  pacf_method:      str='ywm',
+                                  acf_color:        str='blue', 
+                                  pacf_color:       str='blue', 
+                                  title_size:       float|int=14, 
+                                  label_size:       float|int=12
+                                  ) -> plt.figure.Figure:
+    '''
+    Function to generate and return a figure with three subplots: 
+        1) time series data, 
+        2) the autocorrelation function of the data, 
+        3) the partial autocorrelation function of the data.
+
+    Parameters
+    ----------
+    t : np.ndarray
+        DESCRIPTION: array of input times/dates.
+    x : np.ndarray
+        DESCRIPTION: array of input data.
+    acf_lags : int|list|None, optional
+        DESCRIPTION. The default is None. An int or array of acf lag values, used on horizontal axis. If None, half the time series length is used as lags.
+    pacf_lags : int|list|None, optional
+        DESCRIPTION. The default is None. An int or array of acf lag values, used on horizontal axis. If None, acf_lags is used as lags.
+    alpha : float, optional
+        DESCRIPTION. The default is 0.05. If a number is given, the confidence intervals for the given level are returned. For instance if alpha=.05, 95 % confidence intervals are returned where the standard deviation is computed according to Bartlett’s formula. If None, no confidence intervals are plotted.
+    use_vlines : bool, optional
+        DESCRIPTION. The default is True. If True, vertical lines and markers are plotted. If False, only markers are plotted.
+    adjusted : bool, optional
+        DESCRIPTION. The default is False. For acf, if True, then denominators for autocovariance are n-k, otherwise n
+    fft : bool, optional
+        DESCRIPTION. The default is False. For acf, if True, computes the ACF via FFT.
+    missing : str, optional
+        DESCRIPTION. The default is 'none'. A string in [‘none’, ‘raise’, ‘conservative’, ‘drop’] specifying how the NaNs are to be treated.
+    zero : bool, optional
+        DESCRIPTION. The default is True. Flag indicating whether to include the 0-lag autocorrelation. 
+    auto_ylims : bool, optional
+        DESCRIPTION. The default is False. If True, adjusts automatically the y-axis limits to ACF values.
+    bartlett_confint : bool, optional
+        DESCRIPTION. The default is True. Confidence intervals for ACF values are generally placed at 2 standard errors around r_k. The formula used for standard error depends upon the situation. If the autocorrelations are being used to test for randomness of residuals as part of the ARIMA routine, the standard errors are determined assuming the residuals are white noise. The approximate formula for any lag is that standard error of each r_k = 1/sqrt(N). See section 9.4 of [1] for more details on the 1/sqrt(N) result. For more elementary discussion, see section 5.3.2 in [2]. For the ACF of raw data, the standard error at a lag k is found as if the right model was an MA(k-1). This allows the possible interpretation that if all autocorrelations past a certain lag are within the limits, the model might be an MA of order defined by the last significant autocorrelation. In this case, a moving average model is assumed for the data and the standard errors for the confidence intervals should be generated using Bartlett’s formula. For more details on Bartlett formula result, see section 7.2 in [1]. [1] Brockwell and Davis, 1987. Time Series Theory and Methods [2] Brockwell and Davis, 2010. Introduction to Time Series and Forecasting, 2nd edition.
+    pacf_method : str, optional
+        DESCRIPTION. The default is 'ywm'. Specifies which method for the calculations to use:
+                                            “ywm” or “ywmle” : Yule-Walker without adjustment. Default.
+                                            “yw” or “ywadjusted” : Yule-Walker with sample-size adjustment in denominator for acovf. Default.
+                                            “ols” : regression of time series on lags of it and on constant.
+                                            “ols-inefficient” : regression of time series on lags using a single common sample to estimate all pacf coefficients.
+                                            “ols-adjusted” : regression of time series on lags with a bias adjustment.
+                                            “ld” or “ldadjusted” : Levinson-Durbin recursion with bias correction.
+                                            “ldb” or “ldbiased” : Levinson-Durbin recursion without bias correction.
+    acf_color : str, optional
+        DESCRIPTION. The default is 'blue'. Colour of the acf plot markers.
+    pacf_color : str, optional
+        DESCRIPTION. The default is 'blue'. Colour of the pacf plot markers.
+    title_size : float|int, optional
+        DESCRIPTION. The default is 14. 
+    label_size : float|int, optional
+        DESCRIPTION. The default is 12.
+
+    Returns
+    -------
+    fig : matplotlib figure
+        DESCRIPTION. Figure of time series, acf, pacf.
+
+    '''
+    
+    import numpy as np
+    import statsmodels.graphics.tsaplots as tsaplots
+    
+    #if the acf lags are not set then take as half the time series length
+    if acf_lags is None:
+        acf_lags = int(np.floor(len(x)/2))
+    #if the pacf lags are not set, take as the same as acf    
+    if pacf_lags is None:
+        pacf_lags = acf_lags
+    
+    # Create a figure and axes
+    fig, axs = plt.subplots(3, 1, figsize=(10, 15), sharex=True)
+    
+    # Plot raw time series data
+    axs[0].plot(t, x, label='Time Series Data', color='black')
+    axs[0].set_title('Raw Time Series Data', fontsize=title_size)
+    axs[0].set_ylabel('Value', fontsize=label_size)
+    axs[0].legend()
+    
+    # Plot ACF
+    tsaplots.plot_acf(x, 
+                      lags=acf_lags, 
+                      ax=axs[1], 
+                      color=acf_color, 
+                      alpha=alpha,
+                      use_vlines=use_vlines,
+                      adjusted=adjusted,
+                      fft=fft,
+                      missing=missing,
+                      zero=zero,
+                      auto_ylims=auto_ylims, 
+                      bartlett_confint=bartlett_confint
+                      )
+    axs[1].set_title('Autocorrelation Function (ACF)', fontsize=title_size)
+    
+    # Plot PACF
+    tsaplots.plot_pacf(x, 
+                       lags=pacf_lags, 
+                       ax=axs[2], 
+                       color=pacf_color,
+                       alpha=alpha,
+                       method = pacf_method,
+                       use_vlines=use_vlines,
+                       zero=zero)
+    axs[2].set_title('Partial Autocorrelation Function (PACF)', fontsize=title_size)
+    
+    # Set common labels
+    for ax in axs:
+        ax.set_xlabel('Time', fontsize=label_size)
+    
+    plt.tight_layout()
+    
+    # Return the figure object
+    return fig
+    
